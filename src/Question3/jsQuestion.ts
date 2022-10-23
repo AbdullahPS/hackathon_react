d449a231a78947a5bd4f77ef4d05dae09ddfdb61
AbdullahPS <abdullahtabbal@outlook.com>
2022-10-23 17:19:57 +0300

    Task 3 solution

--- src/Question3/jsQuestion.ts
+++ src/Question3/jsQuestion.ts
@@ -1,3 +1,22 @@
-export const transformValue = (val: unknown): unknown => {
-    return val;
+
+export const transformValue = (val: unknown ):unknown => {
+const solution={...val as Object}
+const a:Record<string, unknown>={};
+for (const [key,value] of Object.entries(solution) ){
+ 
+        a[key]=doTransformation(value)
+    
+}
+
+    return a;
+}
+
+const doTransformation=(value:(string|number|Object)):(string|number|Object)=>{
+    if(typeof value==="number")
+    return value+1
+    else if (typeof value==="string")
+    return value+" AE"
+    else {
+        return Object.values(value).map(child=>doTransformation(child))
+    }
 }
